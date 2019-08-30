7769b94d9b90c2a0a6dd9159b8593f36413ded5d
Riccardo Zanella <riccardo.zanella@gmail.com>
2019-08-30 20:43:54 +0200

    preparing template for logistic regression

--- tensorflow/Geron-Hands-On/09-Tensorflow/regression.py
+++ tensorflow/Geron-Hands-On/09-Tensorflow/regression.py
@@ -25,7 +25,6 @@ class DataBatch:
             y_batch = y[indices]
             yield X_batch, y_batch
 
-
 class LinearRegression:
     def __init__(self, learning_rate=1e-2, batch_size=100, n_epochs=2000, print_every=200):
         self._learning_rate = learning_rate
@@ -102,3 +101,77 @@ class LinearRegression:
                 predictions = sess.run(pred, feed_dict={tX: X, theta: self._theta})
 
         return predictions
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate=1e-2, batch_size=100, n_epochs=2000, print_every=200):
+        self._learning_rate = learning_rate
+        self._batch_size = batch_size
+        self._n_epochs = n_epochs
+        self._print_every = print_every
+        self._theta = None
+
+    def fit(self, X, y):
+        self._n_features = X.shape[1]
+        data = DataBatch(self._batch_size)
+
+        with tf.Graph().as_default():
+            # placeholders and variables
+            # tX = tf.placeholder(tf.float32, shape=(None, self._n_features), name='X')
+            # ty = tf.placeholder(tf.float32, shape=(None, 1), name='y')
+            # theta = tf.Variable(tf.zeros([self._n_features, 1]), name='theta')
+            
+            # initialization step
+            # init = tf.global_variables_initializer()
+
+            # evaluate predictions
+            # TODO
+            
+            # TODO change objective function according to above code
+            # optimizer = tf.train.GradientDescentOptimizer(learning_rate=self._learning_rate)
+            # training_op = optimizer.minimize(mse, name='training_op')
+            
+
+            # summary
+            # TODO change objective function according to above code
+            # mse_summary = tf.summary.scalar('MSE', mse)
+            # file_writer = tf.summary.FileWriter('tmp', tf.get_default_graph())
+
+            # saver = tf.train.Saver()
+            
+            # with tf.Session() as sess:
+                
+            #     sess.run(init)
+            #     for epoch in range(self._n_epochs):
+            #         for X_batch, y_batch in data.batch(X, y, epoch):
+            #             _, mse_batch = sess.run([training_op, mse], feed_dict={tX: X_batch, ty: y_batch} )
+
+            #         if epoch % self._print_every == 0 or epoch == self._n_epochs-1:
+            #             print('Epoch {: 5d}, MSE {:4.2e}'.format(epoch, mse_batch))
+                        
+            #             save_path = saver.save(sess, 'tmp/model.ckpt')
+            #             file_writer.add_summary(mse_summary.eval(feed_dict={tX: X_batch, ty: y_batch}), epoch)
+
+            #     self._theta = theta.eval()
+            #     save_path = saver.save(sess, 'tmp/model_final.ckpt')
+            
+            #     file_writer.close()
+        return self
+    
+    def predict(self, X):
+        if self._theta is None:
+            raise NotFittedError()
+        
+        if X.shape[1] != self._n_features:
+            raise ShapesNotCompatibleError()
+        
+        # graph = tf.Graph()
+        # with graph.as_default():
+        #     tX = tf.placeholder(tf.float32, shape=(None, self._n_features), name='X')
+        #     theta = tf.placeholder(tf.float32, shape=(self._n_features, 1), name='theta')
+        #     pred = tf.matmul(tX, theta)
+
+        #     with tf.Session() as sess:
+        #         predictions = sess.run(pred, feed_dict={tX: X, theta: self._theta})
+
+        # return predictions
